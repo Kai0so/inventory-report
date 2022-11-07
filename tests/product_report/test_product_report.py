50ebb754792202ba5e69b49cbddcdd09137c51a4
Kai0so <kaioaugustoalmeida.m@gmail.com>
2022-11-07 16:56:17 -0300

    test: Testa o relatório individual do produto

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,19 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        1,
+        "xablau",
+        "xablauzinho",
+        "23/01/1998",
+        "23/01/2100",
+        "2345678",
+        "nenhuma observaçao",
+    )
+
+    assert str(product) == (
+        "O produto xablau fabricado em 23/01/1998 por xablauzinho "
+        "com validade até 23/01/2100 precisa ser armazenado "
+        "nenhuma observaçao."
+    )
